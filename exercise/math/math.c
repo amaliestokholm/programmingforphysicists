174822ebf5151e95da9e1cc0b8b008b7eccbd330
Amalie Stokholm <stokholm@post.au.dk>
2017-02-14 11:28:02 +0100

    Fix math

--- exercise/math/math.c
+++ exercise/math/math.c
@@ -3,6 +3,14 @@
 #include <complex.h>
 
 
+void significantdigits(){
+	float f = 1.0f / 9;
+	double d = 1.0 / 9;
+	long double l = 1.0L / 9;
+	printf("Float \t\t%.25g\nDouble \t\t%.25lg\nLong double \t%.25Lg\n", f, d, l);
+}
+
+
 int main() {
 	// Calculate gamma(5)
 	double x=5;
@@ -22,5 +30,6 @@ int main() {
 	printf("exp(%g+%gi) is %g+%gi\n", creal(I * M_PI), cimag(I * M_PI), creal(cpi), cimag(cpi));
 	double complex ie = cpow(I, M_E); 
 	printf("i^e is %g+%gi\n", creal(ie), cimag(ie));
-return 0;
+	significantdigits();
+	return 0;
 }
