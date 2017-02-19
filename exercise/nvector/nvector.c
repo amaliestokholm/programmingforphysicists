d529205253f1fcec69957a5fb75583a238e37f5c
Amalie Stokholm <stokholm@post.au.dk>
2017-02-19 17:08:23 +0100

    Add nvector

--- exercise/nvector/nvector.c
+++ exercise/nvector/nvector.c
@@ -12,8 +12,23 @@ nvector* nvector_alloc(int n){
 	return v;
 }
 
-void nvector_free(nvector* v){ free(v->data); free(v); v=NULL; }
+void nvector_free(nvector* v){
+	// Free the memory v points to. You need a free statement per malloc. Unused pointers are set to NULL in order to protect against dangling pointer bugs. If v is accessed after it is freed, you may read or overwrite random memory, but if it is a null pointer, most systems would crash, telling you what the error is.
+       	free(v->data);
+	free(v);
+	v=NULL;
+}
 
-void nvector_set(nvector* v, int i, double value){ (*v).data[i]=value; }
+void nvector_set(nvector* v, int i, double value){
+	// make sure that i is within the length of (*v).data
+	assert(0 <= i && i < (*v).size)
+        (*v).data[i]=value;
+}
 
-double nvector_get(nvector* v, int i){return (*v).data[i]; }
+double nvector_get(nvector* v, int i){
+	// make sure that i is within the length of (*v).data
+	assert(0 <= i && i < (*v).size)
+	return (*v).data[i]; 
+}
+
+
