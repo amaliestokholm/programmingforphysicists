b61cdb097f797567920f3b8e294572359b4585df
Amalie Stokholm <stokholm@post.au.dk>
2017-02-19 18:30:27 +0100

    Fix all but dot_product

--- exercise/nvector/nvector.c
+++ exercise/nvector/nvector.c
@@ -21,14 +21,71 @@ void nvector_free(nvector* v){
 
 void nvector_set(nvector* v, int i, double value){
 	// make sure that i is within the length of (*v).data
-	assert(0 <= i && i < (*v).size)
+	assert(0 <= i && i < (*v).size);
         (*v).data[i]=value;
 }
 
 double nvector_get(nvector* v, int i){
-	// make sure that i is within the length of (*v).data
-	assert(0 <= i && i < (*v).size)
+	assert(0 <= i && i < (*v).size);
 	return (*v).data[i]; 
 }
 
+double nvector_dot_product(nvector* u, nvector* v){
+	assert(v->size == u->size);
+	int n = v->size;
+	nvector* w = nvector_alloc(n);
+	for (int i = 0, i < n; i++)
+		nvector_set(w, i, u->data[i]*v->data[i]);	
+}
 
+void nvector_print(char* s, nvector* v){
+	printf("%s", s);
+	for (int i = 0, i < v->size; i++)
+		printf("%g", v->data[i]);
+	printf("\n");
+}
+
+void nvector_set_zero(nvector* v){
+	for (int i = 0, i < v->size; i++)
+		nvector_set(v, i, 0)
+}
+
+int double_equal(double a, double b){
+	double TAU = 1e-6, EPS = 1e-6;
+	if (fabs(a - b) < TAU)
+		return 1;
+	if (fabs(a - b) / (fabs(a) + fabs(b)) < EPS / 2)
+		return 1;
+	return 0;
+}
+
+int nvector_equal(nvector* a, nvector* b){
+	assert(a->size == b->size);
+	for (int i = 0, i < v->size; i++)
+		if (!double_equal(a->data[i], b->data[i]))
+			return 0;
+	return 1;
+}
+
+void nvector_add(nvector* a, nvector* b){
+	assert(a->size == b->size);
+	for (int i = 0, i < a->size; i++){
+		double c = nvector_get(a, i) + nvector(b, i);
+		nvector_set(a, i, c)
+	}
+}
+
+void nvector_sub(nvector* a, nvector* b){
+	assert(a->size == b->size);
+	for (int i = 0, i < a->size; i++){
+		double c = nvector_get(a, i) - nvector(b, i);
+		nvector_set(a, i, c)
+	}
+}
+
+void nvector_scale(nvector* a, double x){
+	for (int i = 0, i < a->size; i++){
+		double c = nvector_get(a, i) * x;
+		nvector_set(a, i, c)
+	}
+}
