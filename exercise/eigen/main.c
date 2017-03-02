ac9255a7e7d1e55eb9ebd6e019d9319e2de857e6
Amalie Stokholm <stokholm@post.au.dk>
2017-03-02 17:58:46 +0100

    Almost done with eigen

--- exercise/eigen/main.c
+++ exercise/eigen/main.c
@@ -3,7 +3,7 @@
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_permutation.h>
 #include <gsl/gsl_eigen.h>
-
+const double pi = 3.14159265358979323844;
 int main() {
 	// part a
 	// Declare pointer variables for Ax=b
@@ -38,8 +38,8 @@ int main() {
 	x = gsl_vector_alloc(3);
 	p = gsl_permutation_alloc(3);
 
-	int s;
-	gsl_linalg_LU_decomp(A, p, &s);
+	int m;
+	gsl_linalg_LU_decomp(A, p, &m);
 	gsl_linalg_LU_solve(A, p, b, x);
 
 	printf("x is then found to be:\n");
@@ -63,11 +63,31 @@ int main() {
 	for(int i=0; i < n-1; i++) {
 		gsl_matrix_set(H, i, i, -2);
 		gsl_matrix_set(H, i, i+1, 1);
-		gls_matrix_set(H, i+1, i, 1);
+		gsl_matrix_set(H, i+1, i, 1);
 	}
-	gsl_matrix(set, n-1, n-1, -2);
-	double s=1.0/(n+1);
+	gsl_matrix_set(H, n-1, n-1, -2);
+	double s = 1.0 / (n+1);
 	gsl_matrix_scale(H, -1/s/s);
+
+	// Diagonalize
+	gsl_eigen_symmv_workspace* w =  gsl_eigen_symmv_alloc(n);
+	gsl_vector* eval = gsl_vector_alloc(n);
+	gsl_matrix* evec = gsl_matrix_calloc(n, n);
+	gsl_eigen_symmv(H, eval, evec, w);
+
+	gsl_eigen_symmv_sort(eval, evec, GSL_EIGEN_SORT_VAL_ASC);
+
+	fprintf (stderr, "i   exact   calculated\n");
+	for (int k = 0; k < n/3; k++){
+		double exact = pi * pi * (k + 1) * (k + 1);
+		double calculated = gsl_vector_get(eval, k);
+		fprintf(stderr, "%i %g %g\n", k, exact, calculated);
+	}
+
+	// Free memory
+	gsl_matrix_free(H);
+	gsl_vector_free(eval);
+	gsl_matrix_free(evec);
 
 	return 0;
 }
