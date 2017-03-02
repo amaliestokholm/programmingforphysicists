9815f2ef4dda01b0f912c1ba907c273130a8ea4b
Amalie Stokholm <stokholm@post.au.dk>
2017-03-02 13:19:53 +0100

    Finish gnuplot

--- exercise/gnuplot/main-cmdline.c
+++ exercise/gnuplot/main-cmdline.c
@@ -2,9 +2,10 @@
 #include <math.h>
 #include <stdlib.h>
 
-void main(int argc, char** argv) {
+int main(int argc, char** argv) {
 	for (int i=1;i<argc;i++) {
 		double x=atof(argv[i]);
 		printf("%lg \t %lg\n", x, sin(x));
 	}
+	return 0;
 }
