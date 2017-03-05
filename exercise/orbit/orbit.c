8cef8319060e69cae4c7aff5fe8955c34ca49c7f
Amalie Stokholm <stokholm@post.au.dk>
2017-03-05 18:59:39 +0100

    First part done

--- exercise/orbit/orbit.c
+++ exercise/orbit/orbit.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <gsl/gsl_odeiv2.h>
+#include <gsl/gsl_errno.h>
 
-int logistic_function() {
-	dydt[0] = y[1];
-	dydt[1] = y[0] * (1 - y[0]);
+int logistic_function(double t, double y[], double dydt[]) {
+	dydt[0] =  y[0] * (1 - y[0]);
 	// An ODE system should return GSL_SUCCES if the calculation was completed succesfully.
 	return GSL_SUCCESS;
 }
@@ -13,7 +13,7 @@ double my_function(double x) {
 	gsl_odeiv2_system sys;
 	sys.function = logistic_function;
 	sys.jacobian = NULL;
-	sys.dimenstion = 2;
+	sys.dimension = 1;
 	sys.params = NULL;
 	
 	double epsabs = 1e-6, epsrel = 1e-6;
@@ -21,19 +21,22 @@ double my_function(double x) {
 	gsl_odeiv2_driver *driver = gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rkf45,
 		       	hstart, epsabs, epsrel);
 
-	double t=0, y[2] = {0,1};
+	double t=0, y[1] = {0.5};  // initial conditions
 	gsl_odeiv2_driver_apply(driver, &t, x, y);
 
 	gsl_odeiv2_driver_free(driver);
 	return y[0];
 }
 
-double math_function(double x) {
-	return 1 / (1 + exp(x));
+double math_function(double t) {
+	return 1.0 / (1 + exp(-t));
+}
 
 int main() {
-	double start=0, end=3, dx = 0.5;
-	for (double x=start, x < end + dx; x += dx) {
-		printf("%g %g %g\n", x, my_function(x), math_function(x));
+	double start=0.0, end=3.0, dt = 0.1;
+	printf("t\t y(t)\t exact\n");
+	for (double t=start; t <= end; t += dt) {
+		printf("%g %g %g\n", t, my_function(t), math_function(t));
 	}
 }
+
