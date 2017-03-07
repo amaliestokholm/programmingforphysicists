ca8fb29c0353a49deb407fbc5ef40211439660be
Amalie Stokholm <stokholm@post.au.dk>
2017-03-07 10:41:16 +0100

    Fix some of the integrals in part b

--- exercise/integ/integ.c
+++ exercise/integ/integ.c
@@ -18,15 +18,16 @@ int main(void) {
 	double start = 0, end = 1;
 	double epsabs = 1e-6, epsrel = 1e-6;
 	double n = 1000;
-	double result, error;
+	double psiHpsi, pHperror;
+	double psipsi, pperror;
 
 	gsl_integration_workspace * w = gsl_integration_workspace_alloc(n);
 
 	gsl_function F;
 	F.function = &func;
 
-	psiHpsi = gsl_integration_qagi(&F, start, end, epsabs, epsrel, n, w, &result, &error);  // we use the adaptive integration with singularities
-	psipsi = gsl_integration_qagi(&F, start, end, epsabs, epsrel, n, w, &result, &error);  // we use the adaptive integration with singularities
+	gsl_integration_qagi(&F, start, end, epsabs, epsrel, n, w, &psiHpsi, &pHperror); 
+	gsl_integration_qagi(&F, start, end, epsabs, epsrel, n, w, &psipsi, &pperror);
 
 	printf("Result = %.12f\n", result);
 	printf("Estimated error = %.12f\n", error);
