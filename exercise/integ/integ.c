588fdbb28fc579b3b6d7563a8c3dd9b1d2385d82
Amalie Stokholm <stokholm@post.au.dk>
2017-03-07 10:38:39 +0100

    Almost done with numerical integrations

---
+++ exercise/integ/integ.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_integration.h>
+
+double trialhamiltoniantrial(double x, void * params) {
+	double alpha = *(double *) params;
+	double f = ((- alpha * alpha * x * x)/2 + alpha/2 + (x * x)/2) * exp(- alpha * x * x);
+	return f;
+}
+
+double trialtrial(double x, void * params) {
+	double alpha = *(double *) params;
+	double f = exp( - alpha * x * x);
+	return f;
+}
+
+int main(void) {
+	double start = 0, end = 1;
+	double epsabs = 1e-6, epsrel = 1e-6;
+	double n = 1000;
+	double result, error;
+
+	gsl_integration_workspace * w = gsl_integration_workspace_alloc(n);
+
+	gsl_function F;
+	F.function = &func;
+
+	psiHpsi = gsl_integration_qagi(&F, start, end, epsabs, epsrel, n, w, &result, &error);  // we use the adaptive integration with singularities
+	psipsi = gsl_integration_qagi(&F, start, end, epsabs, epsrel, n, w, &result, &error);  // we use the adaptive integration with singularities
+
+	printf("Result = %.12f\n", result);
+	printf("Estimated error = %.12f\n", error);
+
+	gsl_integration_workspace_free(w);
+}
---
+++ exercise/integ/lnsqrt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+#include <gsl/gsl_integration.h>
+
+double func (double x) {
+	double f = (log(x) / sqrt(x));
+	return f;
+}
+
+int main(void) {
+	double start = 0, end = 1;
+	double epsabs = 1e-6, epsrel = 1e-6;
+	double n = 1000;
+	double result, error;
+
+	gsl_integration_workspace * w = gsl_integration_workspace_alloc(n);
+
+	gsl_function F;
+	F.function = &func;
+
+	gsl_integration_qags(&F, start, end, epsabs, epsrel, n, w, &result, &error);  // we use the adaptive integration with singularities
+
+	printf("Result = %.12f\n", result);
+	printf("Estimated error = %.12f\n", error);
+
+	gsl_integration_workspace_free(w);
+}
